b13658e2a3a22e11a4c8f98331a4e2557e277b82
Aditya Kushwaha <146769134+AdityaKushwaha404@users.noreply.github.com>
2025-12-12 11:29:31 +0530

    style: format 500 error page for improved readability

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
 
 export default function Custom500() {
   return (
     <html>
       <body>
-        <div style={{display: 'flex',alignItems: 'center',justifyContent: 'center',height: '100vh',flexDirection: 'column'}}>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            flexDirection: "column",
+          }}
+        >
           <h1>500 — Server error</h1>
           <p>Something went wrong on the server.</p>
         </div>
