823df6221c736d4baebf22419e78c66c16290371
Aditya Kushwaha <146769134+AdityaKushwaha404@users.noreply.github.com>
2025-12-13 22:15:19 +0530

    fix: update redirect URL in CV route to point to the latest document

--- app/cv/route.ts
+++ app/cv/route.ts
@@ -2,6 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(_req: NextRequest) {
   const driveUrl =
-    "https://docs.google.com/document/d/1EDDtcETx_FnMtOQDurEwYGIpb6qi4Ui5Pf87gJYdA6U/edit?usp=sharing";
+    "https://drive.google.com/file/d/1VtFHQUtB3SNb_kKd7J_bAMkhIJYVx8-t/view?usp=sharing";
   return NextResponse.redirect(driveUrl, { status: 302 });
 }
